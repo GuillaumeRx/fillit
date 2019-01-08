5574c13da5073d4e4532c2d10eafdb672bcb792b
Camille DUVERGER <cduverge@e1r12p21.42.fr>
2019-01-08 13:34:07 +0100

    main updated

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: guroux <guroux@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 16:11:50 by cduverge          #+#    #+#             */
-/*   Updated: 2019/01/07 17:31:18 by guroux           ###   ########.fr       */
+/*   Updated: 2019/01/08 13:33:48 by cduverge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,14 @@ int	main(int ac, char **av)
 	if (ac != 2)
 		ft_putendl(USAGE);
 	else
+	{
 		fd = open(av[1], O_RDONLY);
+		if (check_error(fd) == -1)
+		{
+			ft_putendl("error");
+			return(-1);
+		}
+	}
 		/* traiter les info et checker les erreurs */
 	return (0);
 }
