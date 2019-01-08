8e4175b7f9e55b09f12a5f63cb2b85deb452710c
Camille DUVERGER <cduverge@e3r12p21.42.fr>
2019-01-08 17:28:33 +0100

    check error updated

--- check_error.c
+++ check_error.c
@@ -6,13 +6,60 @@
 /*   By: cduverge <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 17:26:04 by cduverge          #+#    #+#             */
-/*   Updated: 2019/01/08 15:45:26 by cduverge         ###   ########.fr       */
+/*   Updated: 2019/01/08 17:28:06 by cduverge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*0 = Faux ou erreur \ 1 = Vrai*/
+
 #include "fillit.h"
 
-int	check_error(int fd)
+static int	read_a_piece(int fd, t_piece *pieces, char *line)
 {
-	
+	int	i;
+	int	j;
+
+	i = 0;
+	if (pieces.next)
+		pieces.next = pieces;
+	else
+		pieces.next = NULL;
+	while (i < 4)
+	{
+		if ((ret = get_next_line(fd, &line)) == -1)
+			return (0);
+		else if (ret == 0)
+			return (1);
+		j = 0;
+		while (line[j] == '.' || line[j] == '#')
+		{
+			board[i][j] = line[j];
+			j++;
+		}
+		if (j != 4 && line[j] != '\n')
+			return (0);
+		board[i][j] = '\0';
+		i++;
+	}
+}
+
+int			check_error(int fd)
+{
+	t_piece	*pieces;
+	int		ret;
+	int		max_piece;
+	char	*line;
+
+	if (fd < 0)
+		return (0);
+	if (!(*pieces = (t_piece *)malloc(sizeof(t_piece))))
+		return (0);
+	max_piece = 0;
+	while (max_piece < 26)
+	{
+		if (read_a_piece(fd, *pieces, line) == 0)
+			return (0);
+		max_piece++;
+	}
+	return (1);
 }
