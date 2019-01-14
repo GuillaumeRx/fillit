d832601a20cbfdd595919b81316e73b112330e6f
Camille DUVERGER <cduverge@e3r9p14.42.fr>
2019-01-14 17:36:03 +0100

    t_block fonctions updated

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: guroux <guroux@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 16:11:50 by cduverge          #+#    #+#             */
-/*   Updated: 2019/01/14 14:10:56 by cduverge         ###   ########.fr       */
+/*   Updated: 2019/01/14 17:35:11 by cduverge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	main(int ac, char **av)
 {
 	t_piece	*pieces;
+	t_piece *cur;
 	int	fd;
 
 	if (ac != 2)
@@ -26,8 +27,15 @@ int	main(int ac, char **av)
 		if (check_error(fd, *pieces) == 0)
 		{
 			ft_putendl("error");
-			return(-1);
+			return(0);
 		}
+		cur = pieces;
+		while (cur)
+		{
+			convert_board_into_block_list(&cur);
+			cur = cur->next;
+		}
+		pieces = cur;
 	}
 		/* traiter les info et checker les erreurs */
 	return (0);
--- part_of_list.c
+++ part_of_list.c
@@ -6,7 +6,7 @@
 /*   By: cduverge <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/09 15:59:05 by cduverge          #+#    #+#             */
-/*   Updated: 2019/01/14 16:47:22 by cduverge         ###   ########.fr       */
+/*   Updated: 2019/01/14 17:34:51 by cduverge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,37 @@ int	part_of_list(t_piece **pieces)
 		(*pieces)->next = NULL;
 	}
 	return (1);
+}
+
+/*passer les donnees du **board dans une liste chainee*/
+
+int	convert_board_into_block_list(t_piece **pieces)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			while ((*pieces)->board[i][j] == '.')
+				j++;
+			if ((*pieces)->board[i][j] == '#')
+			{
+				
+				(*pieces)->pos = get_pos(pieces->pos);
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+t_block	*get_pos(s_block *pos)
+{
+	t_block	*cur;
+
+	if (!())
 }
